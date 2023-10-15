10224493f7120b2b3eda530b777afa12390ac9cf
Abd Alrahman Bany Issa <81553723+banyissaabdalrahman@users.noreply.github.com>
2023-10-15 13:20:57 +0300

    adding recipe-model and manage the recipe list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Name',
+      'A Test Decription',
+      'https://www.modernhoney.com/wp-content/uploads/2018/03/The-Best-Chicken-Marinade-Recipe-1200x856.jpg'
+    ),
+    new Recipe(
+      'A Test Name',
+      'A Test Decription',
+      'https://www.modernhoney.com/wp-content/uploads/2018/03/The-Best-Chicken-Marinade-Recipe-1200x856.jpg'
+    ),
+  ];
 }
