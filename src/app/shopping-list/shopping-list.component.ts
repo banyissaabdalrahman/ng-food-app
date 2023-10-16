a8d813b53636e6037903ad12f04603f43ce4e66a
Abd Alrahman Bany Issa <81553723+banyissaabdalrahman@users.noreply.github.com>
2023-10-16 15:16:10 +0300

    working on shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Ingredient} from "../shared/ingredients.model";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients = [
+    new Ingredient("Apple", 3),
+    new Ingredient("Orange", 5),
+    new Ingredient("Banana", 2),
+  ];
 }
